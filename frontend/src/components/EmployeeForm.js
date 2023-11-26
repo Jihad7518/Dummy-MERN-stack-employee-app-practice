ec2e8466c02aa4cfa1518b557871d6a9050febb0
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-27 05:09:08 +0600

    Update EmployeeForm.js

--- frontend/src/components/EmployeeForm.js
+++ frontend/src/components/EmployeeForm.js
@@ -1,1 +1,27 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+
+const EmployeeForm = () => {
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+
+  const createEmployee = async (data) => {
+
+    const savedUserResponse = await fetch(
+      `${process.env.REACT_APP_BASE_URL}/createUser`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...data }),
+      }
+    );
+
+    console.log("FORM RESPONSE......", savedUserResponse);
+
+    navigate("/")
+  };
 
