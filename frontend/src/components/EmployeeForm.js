93da410fb3d83e6df0072a20c5161ac0e4629109
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-27 05:09:20 +0600

    Update EmployeeForm.js

--- frontend/src/components/EmployeeForm.js
+++ frontend/src/components/EmployeeForm.js
@@ -25,3 +25,24 @@ const EmployeeForm = () => {
     navigate("/")
   };
 
+  return (
+    <div>
+      <form onSubmit={handleSubmit(createEmployee)} className="mt-8">
+        <div className="space-y-5">
+          <div>
+            <label
+              htmlFor="name"
+              className="text-base font-medium text-gray-900 dark:text-gray-200"
+            >
+              {" "}
+              Employee Name{" "}
+            </label>
+            <div className="mt-2.5">
+              <input
+                className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                type="text"
+                placeholder="Enter You Full Name"
+                {...register("name")}
+              ></input>
+            </div>
+          </div>
