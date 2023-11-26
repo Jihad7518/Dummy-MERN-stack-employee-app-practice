e59b732de188fd9aebe5cd985052706e8a5a5a11
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-27 05:10:05 +0600

    Update EmployeeForm.js

--- frontend/src/components/EmployeeForm.js
+++ frontend/src/components/EmployeeForm.js
@@ -46,6 +46,7 @@ const EmployeeForm = () => {
               ></input>
             </div>
           </div>
+
           <div>
             <label
               htmlFor="email"
@@ -81,7 +82,8 @@ const EmployeeForm = () => {
               ></input>
             </div>
           </div>
-         <div>
+
+          <div>
             <label
               htmlFor="department"
               className="text-base font-medium text-gray-900 dark:text-gray-200"
@@ -116,3 +118,33 @@ const EmployeeForm = () => {
               ></input>
             </div>
           </div>
+
+          <div>
+            <button
+              type="submit"
+              className="inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3.5 py-2.5 text-base font-semibold leading-7 text-white hover:bg-indigo-500"
+            >
+              Create Employeee
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="ml-2 h-4 w-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default EmployeeForm;
