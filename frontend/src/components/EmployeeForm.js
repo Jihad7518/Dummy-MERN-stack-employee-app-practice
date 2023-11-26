519c4b47ed9b7ab1ef8f71f18a7280d01114ccce
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-27 05:09:54 +0600

    Update EmployeeForm.js

--- frontend/src/components/EmployeeForm.js
+++ frontend/src/components/EmployeeForm.js
@@ -81,3 +81,38 @@ const EmployeeForm = () => {
               ></input>
             </div>
           </div>
+         <div>
+            <label
+              htmlFor="department"
+              className="text-base font-medium text-gray-900 dark:text-gray-200"
+            >
+              {" "}
+              Employee Department{" "}
+            </label>
+            <div className="mt-2.5">
+              <input
+                className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                type="text"
+                placeholder="Enter Your Employee Department"
+                {...register("department")}
+              ></input>
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="role"
+              className="text-base font-medium text-gray-900 dark:text-gray-200"
+            >
+              {" "}
+              Employee Role{" "}
+            </label>
+            <div className="mt-2.5">
+              <input
+                className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                type="text"
+                placeholder="Enter Your Employee Role"
+                {...register("role")}
+              ></input>
+            </div>
+          </div>
