8a3a52b6d1d866122967cc3be5e5cd54b1fed27d
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-27 05:06:43 +0600

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,1 +1,53 @@
 
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+// const people = [
+//   {
+//     name: "John Doe",
+//     title: "Front-end Developer",
+//     department: "Engineering",
+//     email: "john@devui.com",
+//     role: "Developer",
+//     image:
+//       "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
+//   },
+//   {
+//     name: "Jane Doe",
+//     title: "Back-end Developer",
+//     department: "Engineering",
+//     email: "jane@devui.com",
+//     role: "CTO",
+//     image:
+//       "https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+//   },
+// ];
+
+const HomePage = () => {
+  const [empData, setEmpData] = useState();
+
+  const getAllData = async () => {
+    try {
+      const getPeople = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/getallUsers`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const res = await getPeople.json();
+      setEmpData(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllData();
+  },[]);
+  console.log(empData);
+
+  // console.log(empData);
