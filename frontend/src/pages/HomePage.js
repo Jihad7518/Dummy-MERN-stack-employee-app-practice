9bde4f99e9fcb7c9b6d80e93ca2c61ac8af3335d
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-27 05:07:03 +0600

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -51,3 +51,24 @@ const HomePage = () => {
   console.log(empData);
 
   // console.log(empData);
+  return (
+    <>
+      <section className="container px-4 mx-auto py-4">
+        <div className="flex items-center justify-between">
+          <div>
+            <h2 className="text-lg font-medium text-gray-800 dark:text-white">
+              Employees
+            </h2>
+            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">
+              This is a list of all employees. You can add new employees, edit
+              or delete existing ones.
+            </p>
+          </div>
+          <Link to={"/addemployee"}>
+            <div>
+              <button className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-sm font-semibold leading-7 text-white hover:bg-indigo-500 ">
+                Add Employee
+              </button>
+            </div>
+          </Link>
+        </div>
